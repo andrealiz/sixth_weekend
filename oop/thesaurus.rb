a4fd0f3c88fb836c3bf3b4500a777065d7ab8dbb
Andrealiz Afable <dreafable@gmail.com>
2016-08-23 00:43:00 -0700

    create thesaurus app

--- oop/thesaurus.rb
+++ oop/thesaurus.rb
@@ -12,3 +12,59 @@
 # and which belongs in the Entry class.
 #
 # And... test your functionality using RSpec!
+require 'rspec'
+
+class Thesaurus
+
+words = []
+user_input = gets.chomp
+words << user_input
+words.each do |word|
+  end
+
+  def add
+    words = []
+    user_input = gets.chomp
+    words << user_input
+    words
+  end
+
+  def delete
+
+    
+  end
+
+end
+
+class Entry < Thesaurus
+attr_accessor :word, :synonyms, :antonyms
+
+  def look_up
+    
+  end
+
+end
+
+RSpec.describe Thesaurus do
+  describe 'add' do
+    it 'should add the word into the words array' do
+      word = Thesaurus.new
+      expect(word.add(night)).to eq(true)
+    end
+  end
+
+  describe 'delete' do
+  it 'should add the word into the words array' do
+    word = Thesaurus.new
+    expect(word.delete(night)).to eq(true)
+
+
+RSpec.describe Entry do
+  describe 'look_up' do
+    it 'should return true if the number is a prime number' do
+      synonym = Entry.new
+      expect(synonym.look_up(cookie)).to eq(crackers)
+    end
+  end
+
+
